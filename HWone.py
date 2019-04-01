37eb678591568cbcf6fc8de11ccd2c74a1422203
kyungseo <kyungseo96@naver.com>
2019-04-01 23:29:28 +0900

    두번째 파일 추가

---
+++ HWone.py
@@ -0,0 +1,7 @@
+
+number = int(input("숫자를 입력하세요: "))
+if number%2 ==0:  # 가독성을 위해 기호 양옆으로 띄어쓰기 간격을 맞춰주는 편이 좋습니다.
+                  # if number % 2 == 0: 이런 식으로요
+    print(True)
+else:
+    print(False)
